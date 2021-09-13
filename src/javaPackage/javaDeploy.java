c7a5c5be1894965dd8163e264a6de98722137520
Nishad <nishadgotmare@gmail.com>
2021-09-13 13:22:06 +0530

    Maven-tomcat deployment project files

---
+++ src/javaPackage/javaDeploy.java
@@ -0,0 +1,10 @@
+package javaPackage;
+
+public class javaDeploy {
+
+	public static void main(String[] args) {
+		System.out.println("Deploying a java application to tomcat server using maven packaging");
+
+	}
+
+}
